f0b66c55b57a20b685a32ac2318e741aef8c9956
Md. Mehraj Hossain Akash <mehrajakash81@gmail.com>
2025-01-18 19:39:47 +0600

    folder transfer

---
+++ Tree/Height of a Binary Tree.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+ public:
+     int data;
+     Node *left;
+     Node *right;
+
+     Node(int val){
+        data = val;
+        left = nullptr;
+        right = nullptr;
+     }
+};
+
+int height(Node *root ){
+    if(root == nullptr)
+        return -1;
+
+
+    int lh = height(root->left);
+
+    int rh = height(root->right);
+
+    return max(lh, rh)+1;
+}
+
+int main(){
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout<< height(root);
+
+    return 0;
+}
